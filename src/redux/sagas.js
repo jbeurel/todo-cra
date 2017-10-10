021a176e21962cf927a7a164c4098b712e195ccc
Jonathan <bejonster@gmail.com>
2017-10-10 23:11:41 +0200

    first change with firestore

--- src/redux/sagas.js
+++ src/redux/sagas.js
@@ -1,7 +1,18 @@
-import { takeLatest } from 'redux-saga/effects'
+import firebase from 'firebase';
+require("firebase/firestore");
 
-function* sagas() {
-    yield takeLatest("TEST_ACTION", () => console.log('yeah'));
+firebase.initializeApp({
+    apiKey: 'AIzaSyBnBNZHkxbhcjqDYLsQGZRt8faS2t7buvU',
+    authDomain: 'todo-fb58f.firebaseapp.com',
+    projectId: 'todo-fb58f'
+});
+
+const db = firebase.firestore();
+
+export default function* sagas() {
+    const tags = yield db.collection('tags').get().then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+            console.log(doc.id, doc.data());
+        });
+    });
 }
-
-export default sagas;
