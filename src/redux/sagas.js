cde6c586724d4d5bcc94c2359e1e5a070cebb100
Jonathan <bejonster@gmail.com>
2017-11-15 23:58:29 +0100

    use prettier

--- src/redux/sagas.js
+++ src/redux/sagas.js
@@ -2,57 +2,59 @@ import firebase from 'firebase';
 import { eventChannel, buffers } from 'redux-saga';
 import { call, put, take, all, fork, takeEvery } from 'redux-saga/effects';
 import { tagActions } from 'src/tags/actions';
-require("firebase/firestore");
-
+require('firebase/firestore');
 
 firebase.initializeApp({
-    apiKey: 'AIzaSyBnBNZHkxbhcjqDYLsQGZRt8faS2t7buvU',
-    authDomain: 'todo-fb58f.firebaseapp.com',
-    projectId: 'todo-fb58f'
+  apiKey: 'AIzaSyBnBNZHkxbhcjqDYLsQGZRt8faS2t7buvU',
+  authDomain: 'todo-fb58f.firebaseapp.com',
+  projectId: 'todo-fb58f',
 });
 
 const db = firebase.firestore();
 
 function firebaseChannel() {
-    return eventChannel(emitter => {
+  return eventChannel(emitter => {
+    db.collection('tags').onSnapshot(snapshot => {
+      snapshot.docChanges.forEach(change => {
+        emitter(change);
+      });
+    });
 
-        db.collection('tags').onSnapshot((snapshot) => {
-            snapshot.docChanges.forEach((change) => {
-                emitter(change)
-            })}
-        );
-
-        return() => console.log('coucou stop')
-    }, buffers.expanding(5));
+    return () => console.log('coucou stop');
+  }, buffers.expanding(5));
 }
 
 export function* firebaseSagas() {
-    const chan = yield call(firebaseChannel);
-    while (true) {
-        const change = yield take(chan);
-        if (change.type === "added") {
-            yield put(tagActions.added({id: change.doc.id, ...change.doc.data()}));
-        }
-        if (change.type === "modified") {
-            yield put(tagActions.modified({id: change.doc.id, ...change.doc.data()}));
-        }
-        if (change.type === "removed") {
-            yield put(tagActions.removed({id: change.doc.id, ...change.doc.data()}));
-        }
+  const chan = yield call(firebaseChannel);
+  while (true) {
+    const change = yield take(chan);
+    if (change.type === 'added') {
+      yield put(tagActions.added({ id: change.doc.id, ...change.doc.data() }));
     }
+    if (change.type === 'modified') {
+      yield put(
+        tagActions.modified({ id: change.doc.id, ...change.doc.data() }),
+      );
+    }
+    if (change.type === 'removed') {
+      yield put(
+        tagActions.removed({ id: change.doc.id, ...change.doc.data() }),
+      );
+    }
+  }
 }
 
 function* modifyData(data) {
-    yield db.collection('tags').doc(data.tag.id).set(data.tag);
+  yield db
+    .collection('tags')
+    .doc(data.tag.id)
+    .set(data.tag);
 }
 
 function* modifyDataSagas() {
-    yield takeEvery(tagActions.TAG_MODIFY, modifyData);
+  yield takeEvery(tagActions.TAG_MODIFY, modifyData);
 }
 
 export default function*() {
-    yield all([
-        fork(firebaseSagas),
-        fork(modifyDataSagas)
-    ])
+  yield all([fork(firebaseSagas), fork(modifyDataSagas)]);
 }
--- src/tags/actions.js
+++ src/tags/actions.js
@@ -1,26 +1,26 @@
 export const tagActions = {
-    TAG_MODIFY: 'TAG_MODIFY',
-    TAG_ADDED: 'TAG_ADDED',
-    TAG_MODIFIED: 'TAG_MODIFIED',
-    TAG_REMOVED: 'TAG_REMOVED',
+  TAG_MODIFY: 'TAG_MODIFY',
+  TAG_ADDED: 'TAG_ADDED',
+  TAG_MODIFIED: 'TAG_MODIFIED',
+  TAG_REMOVED: 'TAG_REMOVED',
 
-    modify: tag => ({
-        type: tagActions.TAG_MODIFY,
-        tag
-    }),
+  modify: tag => ({
+    type: tagActions.TAG_MODIFY,
+    tag,
+  }),
 
-    added: tag => ({
-        type: tagActions.TAG_ADDED,
-        tag
-    }),
+  added: tag => ({
+    type: tagActions.TAG_ADDED,
+    tag,
+  }),
 
-    modified: tag => ({
-        type: tagActions.TAG_MODIFIED,
-        tag
-    }),
+  modified: tag => ({
+    type: tagActions.TAG_MODIFIED,
+    tag,
+  }),
 
-    removed: tag => ({
-        type: tagActions.TAG_REMOVED,
-        tag
-    })
+  removed: tag => ({
+    type: tagActions.TAG_REMOVED,
+    tag,
+  }),
 };
