b5ea5131e5d1ab203bcad9b5fbcafa39c9d1cab1
Jonathan Beurel <bejonster@gmail.com>
2017-11-29 23:03:50 +0100

    [feature/simplemde] Use SimpleMDE textarea instead of input

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Tags from 'src/views/components/tags/tags-component'
-import Tasks from 'src/views/components/tasks/tasks-component'
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import "node_modules/simplemde/dist/simplemde.min.css";
+import Tags from "src/views/components/tags/tags-component";
+import Tasks from "src/views/components/tasks/tasks-component";
 
 class App extends Component {
   render() {
@@ -16,9 +17,9 @@ class App extends Component {
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
         <h1>Tags</h1>
-        <Tags/>
+        <Tags />
         <h1>Tasks</h1>
-        <Tasks/>
+        <Tasks />
       </div>
     );
   }
--- src/views/components/tag-form/tag-form.js
+++ src/views/components/tag-form/tag-form.js
@@ -1,29 +1,35 @@
-import React from 'react';
-import { Field, reduxForm } from 'redux-form';
+import React from "react";
+import { Field, reduxForm } from "redux-form";
+import SimpleMDEReact from "react-simplemde-editor";
 
 class TagForm extends React.Component {
+  handleChange = data => console.log("coucou SimpleMDE data", data);
+
+  renderField = field => (
+    <SimpleMDEReact value={field.input.value} onChange={field.input.onChange} />
+  );
 
   render() {
-
     const { handleSubmit, pristine, reset, submitting } = this.props;
 
     return (
-        <form onSubmit={handleSubmit}>
-            <Field
-                name="title"
-                component="input"
-                type="text"
-                placeholder="Title"
-            />
-            <button type="submit" disabled={pristine || submitting}>
-                Save
-            </button>
-            <button type="button" disabled={pristine || submitting} onClick={reset}>
-                Cancel
-            </button>
-        </form>
-    )
-  };
+      <form onSubmit={handleSubmit}>
+        <Field
+          name="title"
+          component={this.renderField}
+          type="text"
+          onChange={this.handleChange}
+          placeholder="Title"
+        />
+        <button type="submit" disabled={pristine || submitting}>
+          Save
+        </button>
+        <button type="button" disabled={pristine || submitting} onClick={reset}>
+          Cancel
+        </button>
+      </form>
+    );
+  }
 }
 
 export default reduxForm()(TagForm);
