33bdbd2603c716c8e0c32e80bec1e480e22205af
Jonathan Beurel <jonathanb@theodo.fr>
2017-12-14 14:18:33 +0100

    [material] Install material-ui, use MuiThemeProvider and add basic button on task Component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
 import logo from "./logo.svg";
 import "./App.css";
 import "node_modules/simplemde/dist/simplemde.min.css";
@@ -8,19 +10,21 @@ import Tasks from "src/views/components/tasks/tasks-component";
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <MuiThemeProvider>
+        <div className="App">
+          <div className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h2>Welcome to React</h2>
+          </div>
+          <p className="App-intro">
+            To get started, edit <code>src/App.js</code> and save to reload.
+          </p>
+          <h1>Tasks</h1>
+          <Tasks />
+          <h1>Tags</h1>
+          <Tags />
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <h1>Tags</h1>
-        <Tags />
-        <h1>Tasks</h1>
-        <Tasks />
-      </div>
+      </MuiThemeProvider>
     );
   }
 }
--- src/views/components/task-form/task-form.js
+++ src/views/components/task-form/task-form.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { RaisedButton, TextField } from 'material-ui';
 
 class TaskForm extends React.Component {
 
@@ -9,11 +10,14 @@ class TaskForm extends React.Component {
 
     return (
         <form onSubmit={handleSubmit}>
-            <Field
-                name="label"
-                component="input"
-                type="text"
-                placeholder="Label"
+          <TextField hintText="Hint Text"/>
+          <RaisedButton label="Cancel" />
+
+          <Field
+            name="label"
+            component="input"
+            type="text"
+            placeholder="Label"
             />
             <button type="submit" disabled={pristine || submitting}>
                 Save
