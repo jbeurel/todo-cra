45d3fe4e5ba9a664a66345cebb8147ad705b2d5e
Jonathan Beurel <jonathanb@theodo.fr>
2017-12-18 19:17:29 +0100

    [material] Upgrade to react 16

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
   render() {
     return (
       <MuiThemeProvider>
-        <div>
+        <React.Fragment>
           <AppBar title="Title"/>
           <Grid fluid>
             <Row>
@@ -25,7 +25,7 @@ class App extends Component {
               </Col>
             </Row>
           </Grid>
-        </div>
+        </React.Fragment>
       </MuiThemeProvider>
     );
   }
