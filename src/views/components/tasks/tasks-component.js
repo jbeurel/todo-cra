96da5fc34a37908ad3f1ede71d9183d04052555a
Jonathan Beurel <jonathanb@theodo.fr>
2017-12-19 18:43:43 +0100

    [material] Add divider on top of tasks list

--- src/views/components/tasks/tasks-component.js
+++ src/views/components/tasks/tasks-component.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { connect } from "react-redux";
+import { Divider } from 'material-ui';
 
 import TaskForm from "src/views/components/task-form";
 import { taskActions } from "src/tasks/actions";
@@ -7,18 +8,21 @@ import { taskActions } from "src/tasks/actions";
 class Tasks extends React.Component {
   render() {
     return (
-      <div>
+      <React.Fragment>
+        <Divider />
         {this.props.tasks.valueSeq().map(task => (
-          <div key={task.id}>
-            {/*<span>{task.id} : {task.label}</span>*/}
-            <TaskForm
-              onSubmit={this.props.saveTask}
-              form={`task-form.${task.id}`}
-              initialValues={task}
-            />
-          </div>
+          <React.Fragment>
+            <div key={task.id}>
+              <TaskForm
+                onSubmit={this.props.saveTask}
+                form={`task-form.${task.id}`}
+                initialValues={task}
+              />
+            </div>
+            <Divider/>
+          </React.Fragment>
         ))}
-      </div>
+      </React.Fragment>
     );
   }
 }
