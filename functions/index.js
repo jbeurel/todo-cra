5d858bf25d2beb5a4bb4102df9e79abc58010132
Jonathan Beurel <bejonster@gmail.com>
2018-01-16 00:14:21 +0100

    Add tags ref to subcollection of a task

--- functions/index.js
+++ functions/index.js
@@ -8,7 +8,7 @@ admin.initializeApp(functions.config().firebase);
 const db = admin.firestore();
 
 exports.tagLinking = functions.firestore
-  .document("/tasks/{tasksId}")
+  .document("/tasks/{taskId}")
   .onWrite(event => {
     const tagTitles = twitter.extractHashtags(event.data.data().label);
     console.log("coucou tagTitles", tagTitles);
@@ -26,20 +26,36 @@ exports.tagLinking = functions.firestore
               .collection("tags")
               .add({ title: tagTitle, body: "" })
               .then(ref => {
-                console.log("coucou ref", ref);
-                console.log("coucou ref.id", ref.id);
+                // console.log("coucou ref", ref);
+                // console.log("coucou ref.id", ref.id);
                 console.log("coucou ref.path", ref.path);
+                db
+                  .collection("tasks")
+                  .doc(event.params.taskId)
+                  .collection("tags")
+                  .add({ ref: ref.path })
+                  .catch(error => {
+                    console.log("coucou 2 error", error);
+                  });
               });
           } else {
-            console.log("coucou snapshot.docs[0].ref", snapshot.docs[0].ref);
-            console.log("coucou snapshot.docs[0].id", snapshot.docs[0].id);
+            // console.log("coucou snapshot.docs[0].ref", snapshot.docs[0].ref);
+            // console.log("coucou snapshot.docs[0].id", snapshot.docs[0].id);
             console.log(
               "coucou snapshot.docs[0].ref.path",
               snapshot.docs[0].ref.path
             );
+            db
+              .collection("tasks")
+              .doc(event.params.taskId)
+              .collection("tags")
+              .add({ ref: snapshot.docs[0].ref.path })
+              .catch(error => {
+                console.log("coucou 2 error", error);
+              });
           }
         })
-        .catch(() => console.log("coucou rien"));
+        .catch(error => console.log("coucou error", error));
     });
 
     return event.data.ref.set(
