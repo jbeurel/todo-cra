34a6a35af26521a81a984d0bd6eb55d9211c75f3
Jonathan Beurel <jonathanb@theodo.fr>
2018-03-23 12:39:32 +0100

    [Todo] Refacto actions

--- src/redux/entities/tags/index.js
+++ src/redux/entities/tags/index.js
@@ -1,1 +1,5 @@
-export { tagsReducer } from './tags.reducer';
+export { tagsReducer } from "./tags.reducer";
+
+// Todo: export action pour éviter au composant d'aller chercher les actions
+// Exemple avec TaskForm.container.js
+// import { taskActions } from "src/redux/entities/tasks/tasks.actions";
